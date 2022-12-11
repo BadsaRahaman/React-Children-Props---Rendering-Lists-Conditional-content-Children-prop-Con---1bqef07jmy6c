157add411d308a84bc8fd82a33e76567e384d96e
BadsaRahaman <117195653+BadsaRahaman@users.noreply.github.com>
2022-12-11 18:42:28 +0530

    app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
-import React , {useState,useEffect} from 'react'
-import '../styles/App.css';
-import ChildComponent from './ChildComponent';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+import ChildComponent from "./ChildComponent";
 const App = () => {
- //code here
+  const [currentValue, setCurrentValue] = useState("");
+  const [squareValue, setSquareValue] = useState(0);
+  useEffect(() => {
+    setSquareValue(Number(currentValue) * Number(currentValue));
+  }, [currentValue]);
 
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
-    </div>
-  )
-}
+      <ChildComponent
+        currentValue={currentValue}
+        setCurrentValue={setCurrentValue}
+      />
 
+      <p id="output">{squareValue}</p>
+    </div>
+  );
+};
 
 export default App;
